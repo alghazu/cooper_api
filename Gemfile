0f006654b241785efff2084a3513dda9d0fb7e76
George_Lewis <georgejlewis@hotmail.com>
2018-05-16 14:40:23 +0200

    add rspec tests, add factory bot user and add controllers

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.7'
 gem 'jbuilder', '~> 2.5'
 gem 'rack-cors', require: 'rack/cors'
+gem 'devise_token_auth'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
+    end
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
 end
