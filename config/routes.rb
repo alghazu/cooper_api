ad6bed2b130e1f71e89ed3ebe6016d13e5f6bace
nader <alghazu@gmail.com>
2018-05-18 17:16:20 +0200

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json }  do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create, :index]
     end
